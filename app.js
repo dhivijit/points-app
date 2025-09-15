d448f33d6fa0ff2ce1d599e934fabd5bbacf1698
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2025-09-16 00:00:49 +0530

    Setup for heroku deployment

--- app.js
+++ app.js
@@ -140,4 +140,5 @@ app.get("/seed", async (req, res) => {
     res.send("Seeded members (idempotent)!");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
